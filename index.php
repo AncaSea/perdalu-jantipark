a55eafa9b437aa1727d7971f881bf71606923c0b
Ancase Rekasae <rekasva2018@gmail.com>
2022-05-23 19:38:54 +0700

    coba index

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if(isset($_GET['pesan'])){
             </form>
             <div class="text-center mt-4 kknt"> 
                 Powered By <br>
-                KKNT STMIK SINUS
+                Ancase
             </div>
         </div>
     </body>
