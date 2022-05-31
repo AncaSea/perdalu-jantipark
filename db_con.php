827f17e69599df54ebc756993e61055243ed0dfa
Ancase Rekasae <rekasva2018@gmail.com>
2022-05-31 22:49:06 +0700

    updet

--- db_con.php
+++ db_con.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost';
-$user = 'rey';
+$user = 'root';
 $pass = '';
 $db = 'db_perdalu';
 
--- page/stok/stok.php
+++ page/stok/stok.php
@@ -158,8 +158,6 @@
               		</div>
           	</section>
       	</section>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function () {
 				$(document).on("click",".li-modal",function(){  
