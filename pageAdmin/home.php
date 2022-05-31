458e94dd177c83089c16f429742f05021cdb3870
Reyvaldo <reyvaldowildan7298@gmail.com>
2022-05-31 21:39:17 +0700

    P

--- pageAdmin/home.php
+++ pageAdmin/home.php
@@ -4,7 +4,8 @@
           <div class="row">
             <div class="col-lg-9">
               <div class="row" style="margin-left:1pc;margin-right:1pc;">
-                <h1>DASHBOARD</h1>
+                <h1 style="display: inline-block;">DASHBOARD</h1>
+                <h4 style="float: right; display: inline-block; margin-top: 2pc"><?php echo date("Y/m/d"); ?></h4>
                 <hr>
                 <?php $income = $lihat -> omset();?>
                 <?php $stok = $lihat -> barang_stok_row();?>
@@ -74,19 +75,5 @@
     RIGHT SIDEBAR CONTENT
     *********************************************************************************************************************************************************** -->                  
                 
-          <div class="col-lg-3 ds">
-            <div id="calendar" class="mb">
-              <div class="panel green-panel no-margin">
-                <div class="panel-body">
-                  <div id="date-popover" class="popover top" style="cursor: pointer; disadding: block; margin-left: 33%; margin-top: -50px; width: 175px;">
-                    <div class="arrow"></div>
-                    <h3 class="popover-title" style="disadding: none;"></h3>
-                    <div id="date-popover-content" class="popover-content"></div>
-                  </div>
-                  <div id="my-calendar"></div>
-                </div>
-              </div>
-            </div><!-- / calendar -->
-            </div><!-- /col-lg-3 -->
           </div><! --/row -->
         </section>
