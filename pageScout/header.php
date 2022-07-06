ff4abb0695a0bcd4af73ecd8a61b617b00536654
Ancase Rekasae <rekasva2018@gmail.com>
2022-07-06 11:31:52 +0700

    title

--- pageScout/header.php
+++ pageScout/header.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kasir PHP</title>
+    <title>Direktur BUMDES</title>
 
     <!-- Bootstrap core CSS -->
     <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
