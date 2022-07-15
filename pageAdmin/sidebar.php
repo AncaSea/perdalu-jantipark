cf8ae9a3052b97e2f1d367021066ed5642f17dcb
Ancase Rekasae <rekasva2018@gmail.com>
2022-07-15 13:08:59 +0700

    mimik

--- pageAdmin/sidebar.php
+++ pageAdmin/sidebar.php
@@ -1,95 +1,81 @@
 <aside>
-        <div id="sidebar"  class="nav-collapse">
-            <!-- sidebar menu start-->
-            <ul class="sidebar-menu" id="nav-accordion">
+    <div id="sidebar" class="nav-collapse">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
 
-                <p class="centered"><a><img src="assets/img/user/logo.png" class="img-rectangle" width="100px" height="100px"></a></p>
-                <h5 class="centered">Admin</h5>
-                <h5 class="centered"><?=$_SESSION['namaadmin']?></h5>
-                  
-                <li class="mt">
-                    <a class="<?php echo isset($_GET['page']) ? $_GET['page']=='dashboard/home' ? 'active' : '' : '' ; ?>" href="admin.php?page=dashboard/home">
-                        <i class="fa fa-dashboard <?php if ($_GET['page']=='dashboard/home') echo 'fa-fade';?>"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
+            <p class="centered"><a><img src="assets/img/user/logo.png" class="img-rectangle" width="100px" height="100px"></a></p>
+            <h5 class="centered">Admin</h5>
+            <h5 class="centered"><?= $_SESSION['namaadmin'] ?></h5>
 
-                <li class="sub-menu">
-                    <a href="javascript:void(0);" class="<?php echo isset($_GET['accordion']) ? $_GET['accordion']=='on' ? 'active' : '' : ''; ?>">
-                        <i class="fa fa-desktop"></i>
-                        <span> Perdagangan Luar <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                    </a>
-                    <ul class="sub">
-                        <li>
-                                <a class="<?php echo isset($_GET['page']) ? $_GET['page']=='dashboard/homeLuar' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>"
-                                    href="admin.php?page=dashboard/homeLuar&accordion=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                        <i class="fa fa-dashboard <?php if ($_GET['page']=='dashboard/homeLuar') echo 'fa-fade';?>"></i>
-                                    <span>Dashboard</span>
-                                </a>
-                        </li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='brg_masuk/brg_masuk' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=brg_masuk/brg_masuk&accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa fa-boxes-stacked <?php if ($_GET['page']=='brg_masuk/brg_masuk') echo 'fa-fade';?>"></i><span>Barang Masuk</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='brg_kembali/brg_kembali' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=brg_kembali/brg_kembali&accordion=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa fa-hand-holding-hand <?php if ($_GET['page']=='brg_kembali/brg_kembali') echo 'fa-fade';?>"></i><span>Barang Kembali</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='kasir/kasirLuar' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=kasir/kasirLuar& accordion=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa fa-cash-register <?php if ($_GET['page']=='kasir/kasirLuar') echo 'fa-fade';?>"></i><span>Kasir</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='lap_penjualan/penjualanLuar' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=lap_penjualan/penjualanLuar&accordion=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fas fa-balance-scale <?php if ($_GET['page']=='lap_penjualan/penjualanLuar') echo 'fa-fade';?>"></i><span>Laporan Penjualan</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='supplier/supplier' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=supplier/supplier&accordion=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa fa-people-carry-box <?php if ($_GET['page']=='supplier/supplier') echo 'fa-fade';?>"></i><span>Supplier</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='user/user' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=user/user&accordion=on&active=yes"style="padding-top: 1px;margin-right: 0.5em; border-width:1px;border-color: #9b6d02;    border-radius: 0px 0px 5px 5px">
-                                    <i class="fa fa-user-group <?php if ($_GET['page']=='user/user') echo 'fa-fade';?>"></i><span>User</span></a></li>
-                    </ul>
-                </li>
-                <li class="sub-menu">
-                    <a href="javascript:void(0);" class="<?php echo isset($_GET['accordion2']) ? $_GET['accordion2']=='on' ? 'active' : '' : ''; ?>">
-                        <i class="fa fa-desktop"></i>
-                        <span> Perdagangan Dalam <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                    </a>
-                    <ul class="sub">
-                        <li>
-                            <a class="<?php echo isset($_GET['page']) ? $_GET['page']=='dashboard/homeDalam' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>"
-                                href="admin.php?page=dashboard/homeDalam&accordion2=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                <i class="fa fa-dashboard <?php if ($_GET['page']=='dashboard/homeDalam') echo 'fa-fade';?>"></i>
-                                <span>Dashboard</span>
-                            </a>
-                        </li>
-                    
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='kasir/kasirDalam' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=kasir/kasirDalam&accordion2=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa fa-cash-register <?php if ($_GET['page']=='kasir/kasirDalam') echo 'fa-fade';?>"></i><span>Kasir</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='menu/menu' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=menu/menu&accordion2=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa-solid fa-clipboard-list <?php if ($_GET['page']=='menu/menu') echo 'fa-fade';?>"></i><span>Menu</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='role/role' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=role/role&accordion2=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fas fa-layer-group fa-3x <?php if ($_GET['page']=='role/role') echo 'fa-fade';?>"></i><span>Role</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='lap_penjualan/penjualanDalam' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ;?>" 
-                                href="admin.php?page=lap_penjualan/penjualanDalam&accordion2=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;border-width:1px;border-color: #9b6d02;border-radius: 0px 0px 5px 5px">
-                                    <i class='fas fa-balance-scale <?php if ($_GET['page']=='lap_penjualan/penjualanDalam') echo 'fa-fade';?>'></i><span>Laporan Penjualan</span></a></li>
-                    </ul>
-                </li>
-                <li class="sub-menu">
-                    <a href="javascript:void(0);" class="<?php echo isset($_GET['accordion3']) ? $_GET['accordion3']=='on' ? 'active' : '' : ''; ?>">
-                        <i class="fa fa-gears"></i>
-                        <span> Backup and Restore <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                    </a>
-                    <ul class="sub">
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='backuprestore/backup-data' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=backuprestore/backup-data&accordion3=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa-solid fa-download" <?php if ($_GET['page']=='backuprestore/restore-data') echo 'fa-fade';?>"></i><span>Backup Database</span></a></li>
-                        <li><a class="<?php echo isset($_GET['page']) ? $_GET['page']=='backuprestore/restore-data' ? $_GET['active']=='yes' ? 'active' : '' : '' : '' ; ?>" 
-                                href="admin.php?page=backuprestore/restore-data&accordion3=on&active=yes"style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
-                                    <i class="fa-solid fa-upload <?php if ($_GET['page']=='backuprestore/restore-data') echo 'fa-fade';?>"></i><span>Restore Database</span></a></li>
-                    </ul>
-                </li>
-            </ul>
-            <!-- sidebar menu end-->
-        </div>
-    </aside>
+            <li class="mt">
+                <a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'dashboard/home' ? 'active' : '' : ''; ?>" href="admin.php?page=dashboard/home">
+                    <i class="fa fa-dashboard <?php if ($_GET['page'] == 'dashboard/home') echo 'fa-fade'; ?>"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:void(0);" class="<?php echo isset($_GET['accordion']) ? $_GET['accordion'] == 'on' ? 'active' : '' : ''; ?>">
+                    <i class="fa fa-desktop"></i>
+                    <span> Perdagangan Luar <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
+                </a>
+                <ul class="sub">
+                    <li>
+                        <a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'dashboard/homeLuar' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=dashboard/homeLuar&accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa fa-dashboard <?php if ($_GET['page'] == 'dashboard/homeLuar') echo 'fa-fade'; ?>"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'brg_masuk/brg_masuk' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=brg_masuk/brg_masuk&accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa fa-boxes-stacked <?php if ($_GET['page'] == 'brg_masuk/brg_masuk') echo 'fa-fade'; ?>"></i><span>Barang Masuk</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'brg_kembali/brg_kembali' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=brg_kembali/brg_kembali&accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa fa-hand-holding-hand <?php if ($_GET['page'] == 'brg_kembali/brg_kembali') echo 'fa-fade'; ?>"></i><span>Barang Kembali</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'kasir/kasirLuar' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=kasir/kasirLuar& accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa fa-cash-register <?php if ($_GET['page'] == 'kasir/kasirLuar') echo 'fa-fade'; ?>"></i><span>Kasir</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'lap_penjualan/penjualanLuar' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=lap_penjualan/penjualanLuar&accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fas fa-balance-scale <?php if ($_GET['page'] == 'lap_penjualan/penjualanLuar') echo 'fa-fade'; ?>"></i><span>Laporan Penjualan</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'supplier/supplier' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=supplier/supplier&accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa fa-people-carry-box <?php if ($_GET['page'] == 'supplier/supplier') echo 'fa-fade'; ?>"></i><span>Supplier</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'user/user' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=user/user&accordion=on&active=yes" style="padding-top: 1px;margin-right: 0.5em; border-width:1px;border-color: #9b6d02;    border-radius: 0px 0px 5px 5px">
+                            <i class="fa fa-user-group <?php if ($_GET['page'] == 'user/user') echo 'fa-fade'; ?>"></i><span>User</span></a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:void(0);" class="<?php echo isset($_GET['accordion2']) ? $_GET['accordion2'] == 'on' ? 'active' : '' : ''; ?>">
+                    <i class="fa fa-desktop"></i>
+                    <span> Perdagangan Dalam <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
+                </a>
+                <ul class="sub">
+                    <li>
+                        <a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'dashboard/homeDalam' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=dashboard/homeDalam&accordion2=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa fa-dashboard <?php if ($_GET['page'] == 'dashboard/homeDalam') echo 'fa-fade'; ?>"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'kasir/kasirDalam' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=kasir/kasirDalam&accordion2=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa fa-cash-register <?php if ($_GET['page'] == 'kasir/kasirDalam') echo 'fa-fade'; ?>"></i><span>Kasir</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'menu/menu' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=menu/menu&accordion2=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa-solid fa-clipboard-list <?php if ($_GET['page'] == 'menu/menu') echo 'fa-fade'; ?>"></i><span>Menu</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'role/role' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=role/role&accordion2=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fas fa-layer-group fa-3x <?php if ($_GET['page'] == 'role/role') echo 'fa-fade'; ?>"></i><span>Role</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'lap_penjualan/penjualanDalam' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=lap_penjualan/penjualanDalam&accordion2=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;border-width:1px;border-color: #9b6d02;border-radius: 0px 0px 5px 5px">
+                            <i class='fas fa-balance-scale <?php if ($_GET['page'] == 'lap_penjualan/penjualanDalam') echo 'fa-fade'; ?>'></i><span>Laporan Penjualan</span></a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:void(0);" class="<?php echo isset($_GET['accordion3']) ? $_GET['accordion3'] == 'on' ? 'active' : '' : ''; ?>">
+                    <i class="fa fa-gears"></i>
+                    <span> Backup and Restore <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
+                </a>
+                <ul class="sub">
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'backuprestore/backup-data' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=backuprestore/backup-data&accordion3=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa-solid fa-download" <?php if ($_GET['page'] == 'backuprestore/restore-data') echo 'fa-fade'; ?>"></i><span>Backup Database</span></a></li>
+                    <li><a class="<?php echo isset($_GET['page']) ? $_GET['page'] == 'backuprestore/restore-data' ? $_GET['active'] == 'yes' ? 'active' : '' : '' : ''; ?>" href="admin.php?page=backuprestore/restore-data&accordion3=on&active=yes" style="padding-top: 1px;margin-right: 0.5em;  border-style: ridge;border-width:1px;border-color: #9b6d02;">
+                            <i class="fa-solid fa-upload <?php if ($_GET['page'] == 'backuprestore/restore-data') echo 'fa-fade'; ?>"></i><span>Restore Database</span></a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
--- perdalam/kasir_page.php
+++ perdalam/kasir_page.php
@@ -263,50 +263,72 @@ if (isset($_GET['pesan'])) {
 							</div>
 						</div> -->
 					<div>
-					<b>Menu Porsi :</b><br>
-						<div id="checkcont" class="btn-group">	
+						<b>Menu Porsi :</b><br>
+						<div id="checkcont" class="btn-group">
 							<div class="btn-group mr-2" role="group" aria-label="First group">
 								<input type="radio" class="menu" name="rb" id="cb1" value="porsi lele goreng" />
 								<label class="nmmenu" for="cb1" style="background: #ff6e21; border:#feb101;">Porsi Lele Goreng</label>
 
 								<input type="radio" class="menu" name="rb" id="cb3" value="porsi kakap goreng" />
-								<label class="nmmenu" for="cb3"style="background:#40af49; border:#feb101;">Porsi Kakap Goreng</label>
+								<label class="nmmenu" for="cb3" style="background:#40af49; border:#feb101;">Porsi Kakap Goreng</label>
 
 								<input type="radio" class="menu" name="rb" id="cb5" value="porsi ayam goreng" />
-								<label class="nmmenu" for="cb5"style="background: #957DFD ; border:#feb101;">Porsi Ayam Goreng</label>
+								<label class="nmmenu" for="cb5" style="background: #957DFD ; border:#feb101;">Porsi Ayam Goreng</label>
 							</div>
 							<div class="btn-group mr-2" role="group" aria-label="Second group">
 								<input type="radio" class="menu" name="rb" id="cb2" value="porsi lele bakar" />
-								<label class="nmmenu" for="cb2"style="background: #ffdfba; border:#feb101;">Porsi Lele Bakar</label>
+								<label class="nmmenu" for="cb2" style="background: #ffdfba; border:#feb101;">Porsi Lele Bakar</label>
 
 								<input type="radio" class="menu" name="rb" id="cb4" value="porsi kakap bakar" />
-								<label class="nmmenu" for="cb4"style="background: #baffc9 ; border:#feb101;">Porsi Kakap Bakar</label>
+								<label class="nmmenu" for="cb4" style="background: #baffc9 ; border:#feb101;">Porsi Kakap Bakar</label>
 
 								<input type="radio" class="menu" name="rb" id="cb6" value="porsi ayam bakar" />
-								<label class="nmmenu" for="cb6"style="background: #bae1ff ; border:#feb101;">Porsi Ayam Bakar</label>
+								<label class="nmmenu" for="cb6" style="background: #bae1ff ; border:#feb101;">Porsi Ayam Bakar</label>
 							</div>
 						</div>
 						<br><b>Paket Bar-bar :</b> <br>
 						<div id="checkcont2" class="btn-group">
 							<div class="btn-group mr-2" role="group" aria-label="First group">
 								<input type="radio" class="menu" name="rb" id="cb1.2" value="paket lele goreng" />
-								<label class="nmmenu" for="cb1.2"style="background: #ff6e21 ; border:#feb101;">Paket Lele Goreng</label>
+								<label class="nmmenu" for="cb1.2" style="background: #ff6e21 ; border:#feb101;">Paket Lele Goreng</label>
 
 								<input type="radio" class="menu" name="rb" id="cb3.2" value="paket kakap goreng" />
-								<label class="nmmenu" for="cb3.2"style="background: #40af49 ; border:#feb101;">Paket Kakap Goreng</label>
+								<label class="nmmenu" for="cb3.2" style="background: #40af49 ; border:#feb101;">Paket Kakap Goreng</label>
 
 								<input type="radio" class="menu" name="rb" id="cb5.2" value="paket ayam goreng" />
-								<label class="nmmenu" for="cb5.2"style="background: #957DFE ; border:#feb101;">Paket Ayam Goreng</label>
+								<label class="nmmenu" for="cb5.2" style="background: #957DFE ; border:#feb101;">Paket Ayam Goreng</label>
 							</div>
 							<div class="btn-group mr-2" role="group" aria-label="Second group">
 								<input type="radio" class="menu" name="rb" id="cb2.2" value="paket lele bakar" />
-								<label class="nmmenu" for="cb2.2"style="background: #ffdfba ; border:#feb101;">Paket Lele Bakar</label>
+								<label class="nmmenu" for="cb2.2" style="background: #ffdfba ; border:#feb101;">Paket Lele Bakar</label>
 
 								<input type="radio" class="menu" name="rb" id="cb4.2" value="paket kakap bakar" />
-								<label class="nmmenu" for="cb4.2"style="background: #baffc9 ; border:#feb101;">Paket Kakap Bakar</label>
+								<label class="nmmenu" for="cb4.2" style="background: #baffc9 ; border:#feb101;">Paket Kakap Bakar</label>
 
 								<input type="radio" class="menu" name="rb" id="cb6.2" value="paket ayam bakar" />
-								<label class="nmmenu" for="cb6.2"style="background:#bae1ff ; border:#feb101;">Paket Ayam Bakar</label>
+								<label class="nmmenu" for="cb6.2" style="background:#bae1ff ; border:#feb101;">Paket Ayam Bakar</label>
+							</div>
+						</div>
+						<br><b>Minuman :</b> <br>
+						<div id="checkcont2" class="btn-group">
+							<div class="btn-group mr-2" role="group" aria-label="First group">
+								<input type="radio" class="menu" name="rb" id="cb1.3" value="es teh" />
+								<label class="nmmenu" for="cb1.3" style="background: #ff6e21 ; border:#feb101;">Es Teh</label>
+
+								<input type="radio" class="menu" name="rb" id="cb2.3" value="teh anget" />
+								<label class="nmmenu" for="cb2.3" style="background: #40af49 ; border:#feb101;">Teh Anget</label>
+
+								<input type="radio" class="menu" name="rb" id="cb3.3" value="es jeruk" />
+								<label class="nmmenu" for="cb3.3" style="background: #957DFE ; border:#feb101;">Es Jeruk</label>
+
+								<input type="radio" class="menu" name="rb" id="cb4.3" value="jeruk anget" />
+								<label class="nmmenu" for="cb4.3" style="background: #ffdfba ; border:#feb101;">Jeruk Anget</label>
+
+								<input type="radio" class="menu" name="rb" id="cb5.3" value="es lemontea" />
+								<label class="nmmenu" for="cb5.3" style="background: #ffdfba ; border:#feb101;">Es Lemontea</label>
+
+								<input type="radio" class="menu" name="rb" id="cb6.3" value="lemontea anget" />
+								<label class="nmmenu" for="cb6.3" style="background: #ffdfba ; border:#feb101;">Lemontea Anget</label>
 							</div>
 						</div>
 					</div>
